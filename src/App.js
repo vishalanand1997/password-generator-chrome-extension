3d27b2e0845c4120d431c89fd626adfaebcba3db
Vishal Anand <anandvishal888@gmail.com>
2023-06-09 00:22:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
 import './App.css';
-
+const useStyles = makeStyles({
+  root: {
+    minWidth: 50,
+    textAlign: "center"
+  },
+  title: {
+    fontSize: 14,
+  }
+});
 function App() {
+  const classes = useStyles();
+  const [password, setPassword] = React.useState("");
+  const [copyStatus, setCopyStatus] = React.useState("")
+  React.useEffect(() => {
+    var pass = Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2);
+    let newPassword = pass.substr(0, 15);
+    setPassword(newPassword)
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography variant="h6">
+          Password Generator
+        </Typography>
+        <Typography variant='p' style={{ fontSize: 12 }}>Get random text of 15 character long.</Typography>
+        <div className='password__container'>
+          <span className='password_copText'>{password}</span>
+          <span className='password_copyIcon'><FileCopyIcon style={{ fontSize: 15, cursor: "pointer" }} onClick={() => { navigator.clipboard.writeText(password); setCopyStatus('Copied') }} /></span>
+          {copyStatus !== "" &&
+            <span className="password_copyStatus">{copyStatus}</span>
+          }
+        </div>
+      </CardContent>
+    </Card>
   );
 }
 
